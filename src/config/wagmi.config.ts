5a1b2bfd942280f2dc2c2b728f7384e8c5d6da68
Irsyaad Budi <irsyaad@privyid.tech>
2025-03-07 12:49:38 +0700

    deploy: add napnad

--- src/config/wagmi.config.ts
+++ src/config/wagmi.config.ts
@@ -2,7 +2,7 @@ import { getDefaultConfig } from "@rainbow-me/rainbowkit";
 import {  sepolia } from "wagmi/chains";
 
 const config = getDefaultConfig({
-  appName: "Snoorefi",
+  appName: "NapNad",
   projectId: import.meta.env.VITE_PROJECT_ID,
   chains: [sepolia],
   ssr: false,
